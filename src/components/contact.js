24f11501d2838cca7845abd86ad609399fa9f12f
Brittany Joiner <brittanyjoiner15@gmail.com>
2020-06-02 15:20:03 -0500

    adding a form to interact with netlify

--- src/components/contact.js
+++ src/components/contact.js
@@ -8,9 +8,38 @@ export class Contact extends React.Component {
           <h2>Want to work together?</h2>
         </div>
         <div className="row justify-content-center">
-          <h4>Send an email to brittanyjoiner15@gmail.com</h4>
+          <h4>
+            Send an email to brittanyjoiner15@gmail.com or fill out this form
+            below.
+          </h4>
         </div>
-        <hr />
+
+        <div>
+          <form name="contact" method="POST" data-netlify="true">
+            <div className="form-row mt-3 justify-content-center">
+              <div className="col-md-7">
+                <textarea
+                  className="form-control"
+                  name="message"
+                  placeholder="Add your message here!"
+                ></textarea>
+              </div>
+            </div>
+            <div className="form-row mt-3 justify-content-center">
+              <div className="col-md-4">
+                <input
+                  className="form-control form-control-sm"
+                  type="text"
+                  placeholder="email"
+                ></input>
+              </div>
+            </div>
+            <button type="submit" className="btn btn-primary m-3">
+              Submit
+            </button>
+          </form>
+          <hr />
+        </div>
       </div>
     );
   }
